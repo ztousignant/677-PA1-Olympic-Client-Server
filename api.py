338f17f62bf9a5bd0dcd8d7c72025dbd79817b0e
Zach Tousignant <ztous77@gmail.com>
2018-02-27 12:57:34 -0500

    implemented threads and locks

--- api.py
+++ api.py
@@ -4,7 +4,11 @@ from restart.resource import Resource
 from restart.exceptions import NotFound
 from restart.utils import make_location_header
 
+import threading
+
 api = RESTArt()
+
+medals_lock = threading.Lock()
 medals = {
 	'Rome': {
 		'medals': {
@@ -21,6 +25,8 @@ medals = {
 		}
 	}
 }
+
+events_lock = threading.Lock()
 events = {
 	'Curling':{
 		'Rome': 0,
@@ -41,49 +47,84 @@ class getMedalTally(Resource):
     name = 'getMedalTally'
 
     def read(self, request, team):
-    	try:
-            return medals[team]
-        except KeyError:
-            return {"error": "key not found"}
+    	ret = []
+    	t = threading.Thread(target=getMedalTally.getTally, args=(self, request, team, ret))
+    	t.start()
+    	t.join()
+    	return ret[0]
+
+    def getTally(self, request, team, ret):
+		medals_lock.acquire()
+		try:
+			ret.append(medals[team])
+		except KeyError:
+			ret.append({"error": "key not found"})
+		medals_lock.release()
 
 @api.register(prefix='/incrementMedalTally/<string:team>/<string:medal>',pk='<int:auth>')
 class incrementMedalTally(Resource):
-    name = 'incrementMedalTally'
+	name = 'incrementMedalTally'
 
-    def read(self, request, team, medal, auth):
-        if auth is 123:
-            try:
-				medals[team]['medals'][medal] = medals[team]['medals'][medal]+1
-				return 'Success'
-            except KeyError:
-                return {"error": "key not found"}
-        else:
+	def read(self, request, team, medal, auth):
+		if auth is 123:
+			ret = []
+			t = threading.Thread(target=incrementMedalTally.incrementTally, args=(self, request, team, medal, auth, ret))
+			t.start()
+			t.join()
+			return ret[0]
+		else:
 			return {"error": "unauthorized access"}
+
+	def incrementTally(self, request, team, medal, auth, ret):
+		medals_lock.acquire()
+		try:
+			medals[team]['medals'][medal] = medals[team]['medals'][medal]+1
+			ret.append('Success')
+		except KeyError:
+			ret.append({"error": "key not found"})
+		medals_lock.release()
+
 @api.register(pk='<string:event>')
 class getScore(Resource):
-    name = 'getScore'
+	name = 'getScore'
+	def read(self, request, event):
+		ret = []
+		t = threading.Thread(target=getScore.getScore, args=(self, request, event, ret))
+		t.start()
+		t.join()
+		return ret[0]
 
-    def read(self, request, event):
-    	try:
-            return events[event]
-        except KeyError:
-            return {"error": "key not found"}
+	def getScore(self, request, event, ret):
+		events_lock.acquire()
+		try:
+			ret.append(events[event])
+		except KeyError:
+			ret.append({"error": "key not found"})
+		events_lock.release()
 
 @api.register(prefix='/setScore/<string:event>/<int:rome_score>/<int:gaul_score>',pk='<int:auth>')
 class setScore(Resource):
-    name = 'setScore'
+	name = 'setScore'
 
-    def read(self, request, event, rome_score, gaul_score, auth):
-        if auth is 123:
-            try:
-				events[event]['Rome'] = rome_score
-				events[event]['Gaul'] = gaul_score
-				return 'Success'
-            except KeyError:
-                return {"error": "key not found"}
-        else:
+	def read(self, request, event, rome_score, gaul_score, auth):
+		if auth is 123:
+			ret = []
+			t = threading.Thread(target=setScore.setScore, args=(self, request, event, rome_score, gaul_score, auth, ret))
+			t.start()
+			t.join()
+			return ret[0]
+		else:
 			return {"error": "unauthorized access"}
 
+	def setScore(self, request, event, rome_score, gaul_score, auth, ret):
+		events_lock.acquire()
+		try:
+			events[event]['Rome'] = rome_score
+			events[event]['Gaul'] = gaul_score
+			ret.append('Success')
+		except KeyError:
+			ret.append({"error": "key not found"})
+		events_lock.release()
 
 
 
